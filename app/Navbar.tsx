60c4ddee695e0c9c8089e82bde6a32a5366ee6e8
Imran Farooq <imran_farooq7@hotmail.com>
2023-10-22 01:41:33 +0500

    Navbar component completed

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,12 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { PiBugBeetleFill } from "react-icons/pi";
 
 const Navbar = () => {
+	const pathName = usePathname();
+	// console.log(pathName);
+
 	return (
 		<nav className="flex items-center h-14 px-8 justify-between mb-5 border-b">
 			<Link href="/" className="text-3xl">
@@ -9,10 +14,20 @@ const Navbar = () => {
 			</Link>
 			<ul className="flex items-center space-x-5 text-gray-500 ">
 				<li className="hover:text-gray-900">
-					<Link href="/">Dashboard</Link>
+					<Link
+						href="/dashboard"
+						className={`${pathName.includes("dashboard") ? "underline" : ""}`}
+					>
+						Dashboard
+					</Link>
 				</li>
 				<li className="hover:text-gray-900">
-					<Link href="/issues">Issues</Link>
+					<Link
+						href="/issues"
+						className={`${pathName.includes("issues") ? "underline" : ""}`}
+					>
+						Issues
+					</Link>
 				</li>
 			</ul>
 		</nav>
