630df51925141d5dd524b46ab2841c6d8baa97c6
Imran Farooq <imran_farooq7@hotmail.com>
2023-10-29 05:21:05 +0500

    Spinner component added

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,8 +1,10 @@
 "use client";
+import Spinner from "@/app/components/Spinner";
 import { Button, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 
 import SimpleMDE from "react-simplemde-editor";
@@ -12,22 +14,26 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+	const [loading, setLoading] = useState(false);
 	const router = useRouter();
+
 	const {
 		register,
 		control,
 		handleSubmit,
-		formState: { isValid },
+		formState: { errors },
 	} = useForm<IssueForm>();
+	const onSubmit = handleSubmit(async (data) => {
+		try {
+			setLoading(true);
+			await axios.post("/api/issues", data);
+			router.push("/issues");
+		} catch (error) {}
+	});
 	return (
 		<form
 			className="max-w-lg mx-auto space-y-6 flex flex-col"
-			onSubmit={handleSubmit(async (data) => {
-				try {
-					await axios.post("/api/issues", data);
-					router.push("/issues");
-				} catch (error) {}
-			})}
+			onSubmit={onSubmit}
 		>
 			<TextField.Root className="rounded-3xl p-2">
 				<TextField.Input
@@ -41,11 +47,11 @@ const NewIssuePage = () => {
 				render={({ field }) => (
 					<SimpleMDE placeholder="Description" {...field} />
 				)}
-				rules={{ required: "Descripion" }}
+				rules={{ required: "Description" }}
 			/>
 
-			<Button className="w-full" size={"4"} radius="full" disabled={!isValid}>
-				Submit New Issue
+			<Button className="w-full" size={"4"} radius="full" disabled={loading}>
+				Submit New Issue {loading && <Spinner />}
 			</Button>
 		</form>
 	);
