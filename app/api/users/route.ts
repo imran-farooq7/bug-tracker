31ddf53fb9b3fdc187a649169c52cfc993be43b6
Imran Farooq <imran_farooq7@hotmail.com>
2023-11-21 02:15:13 +0500

    created API endpoint for users

---
+++ app/api/users/route.ts
@@ -0,0 +1,7 @@
+import prisma from "@/prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+	const users = await prisma.user.findMany();
+	return NextResponse.json({ users: users }, { status: 200 });
+}
--- app/issues/components/AssigneeSelect.tsx
+++ app/issues/components/AssigneeSelect.tsx
@@ -1,15 +1,34 @@
 "use client";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useEffect, useState } from "react";
 
 type Props = {};
 const AssigneeSelect = (props: Props) => {
+	const [users, setUsers] = useState<User[]>([]);
+	useEffect(() => {
+		const getUsers = async () => {
+			const res = await fetch("http://localhost:3000/api/users");
+			const data = await res.json();
+			// console.log(data, "users from server");
+			setUsers(data.users);
+		};
+		getUsers();
+	}, []);
+	// console.log(users, "users from server");
 	return (
 		<Select.Root>
 			<Select.Trigger placeholder="Assign..." />
 			<Select.Content>
 				<Select.Group>
 					<Select.Label>Suggestions</Select.Label>
-					<Select.Item value="1">Imran</Select.Item>
+					{users.map((user: User) => {
+						return (
+							<Select.Item value={user.name!} key={user.id}>
+								{user.name}
+							</Select.Item>
+						);
+					})}
 				</Select.Group>
 			</Select.Content>
 		</Select.Root>
