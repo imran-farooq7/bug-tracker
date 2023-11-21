32789b12fd61f9207292a8148d6e601de50d5e8e
Imran Farooq <imran_farooq7@hotmail.com>
2023-11-22 02:49:45 +0500

    update the API to assign issue

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -9,9 +9,19 @@ interface Props {
 }
 
 export async function PATCH(req: NextRequest, { params }: Props) {
-	const session = await getServerSession(authOptions);
-	if (!session) return NextResponse.json({}, { status: 401 });
+	// const session = await getServerSession(authOptions);
+	// if (!session) return NextResponse.json({}, { status: 401 });
 	const body = await req.json();
+	if (body.assignedToUserId) {
+		const user = await prisma.user.findUnique({
+			where: {
+				id: body.assignedToUserId,
+			},
+		});
+		if (!user) {
+			return NextResponse.json({ error: "invalid user" }, { status: 400 });
+		}
+	}
 	const issue = await prisma.issue.findUnique({
 		where: {
 			id: params.id,
@@ -29,6 +39,7 @@ export async function PATCH(req: NextRequest, { params }: Props) {
 		data: {
 			title: body.title,
 			description: body.description,
+			assignedToUserId: body.assignedToUserId,
 		},
 	});
 
